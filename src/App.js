324aca31ab3b59ea244884957863da7183f148b4
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2019-02-07 00:52:53 -0800

    feat(bin): run react-scripts in tandem

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import styles from './App.module.css';
+
+const App = () => (
+	<div className={styles.app}>
+		<header className={styles.appHeader}>
+			<img src={logo} className={styles.appLogo} alt="logo" />
+			<p>
+				Edit <code>src/App.web.js</code> and save to reload.
+			</p>
+			<a
+				className={styles.appLink}
+				href="https://reactjs.org"
+				target="_blank"
+				rel="noopener noreferrer">
+				Learn React
+			</a>
+		</header>
+	</div>
+);
+
+export default App;
