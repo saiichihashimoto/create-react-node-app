b32fbc6ba4ba3016f5edcbc5a03b0ca0a77fc371
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2019-02-10 01:41:03 -0800

    feat(mongod): mongod!
    resolves #21

--- bin/react-node-scripts-build.js
+++ bin/react-node-scripts-build.js
@@ -1,49 +1,42 @@
 #!/usr/bin/env node
 const path = require('path');
 const program = require('commander');
-const { spawnSync } = require('child_process');
+const spawnAsync = require('./spawnAsync');
 
 program
 	.option('--no-web')
 	.option('--no-server')
-	.action(({ web, server }) => {
-		if (server) {
-			spawnSync(
-				require.resolve('@babel/cli/bin/babel'),
-				[
-					'src',
+	.action(({ web, server }) => Promise.resolve()
+		.then(() => server && spawnAsync(
+			require.resolve('@babel/cli/bin/babel'),
+			[
+				'src',
 
-					'--out-dir', 'lib',
-					'--config-file', path.resolve(__dirname, 'babel.config.js'),
-					'--copy-files',
-					'--delete-dir-on-start',
-					'--no-babelrc',
-					'--source-maps',
-					'--verbose',
-				],
-				{
-					env: {
-						...process.env,
-						NODE_ENV: 'production',
-					},
-					stdio: [process.stdin, process.stdout, process.stderr],
+				'--out-dir', 'lib',
+				'--config-file', path.resolve(__dirname, 'babel.config.js'),
+				'--copy-files',
+				'--delete-dir-on-start',
+				'--no-babelrc',
+				'--source-maps',
+				'--verbose',
+			],
+			{
+				env: {
+					...process.env,
+					NODE_ENV: 'production',
 				},
-			);
-		}
-		if (web) {
-			spawnSync(
-				require.resolve('react-scripts/bin/react-scripts'),
-				[
-					'build',
-				],
-				{
-					env: {
-						...process.env,
-						SKIP_PREFLIGHT_CHECK: true,
-					},
-					stdio: [process.stdin, process.stdout, process.stderr],
+			},
+		))
+		.then(() => web && spawnAsync(
+			require.resolve('react-scripts/bin/react-scripts'),
+			[
+				'build',
+			],
+			{
+				env: {
+					...process.env,
+					SKIP_PREFLIGHT_CHECK: true,
 				},
-			);
-		}
-	})
+			},
+		)))
 	.parse(process.argv);
--- bin/react-node-scripts-start.js
+++ bin/react-node-scripts-start.js
@@ -1,31 +1,41 @@
 #!/usr/bin/env node
 const path = require('path');
 const program = require('commander');
-const { spawnSync } = require('child_process');
+const { existsSync } = require('fs');
+const { homedir } = require('os');
+const spawnAsync = require('./spawnAsync');
 
 program
 	.option('--mongod')
 	.option('--no-web')
 	.option('--no-server')
-	.action(({ web, server, mongod }) => spawnSync(
-		require.resolve('foreman/nf'),
-		[
-			'start',
+	.action(({ web, server, mongod }) => Promise.resolve()
+		// This triggers it to download the binary
+		.then(() => mongod && !existsSync(path.resolve(homedir(), '.mongodb-prebuilt')) && spawnAsync(
+			require.resolve('mongodb-prebuilt/built/bin/mongod'),
+			[
+				'--version',
+			],
+		))
+		.then(() => spawnAsync(
+			require.resolve('foreman/nf'),
+			[
+				'start',
 
-			'--procfile', path.resolve(__dirname, 'Procfile'),
-			Object.entries({ web, server, mongod })
-				.map(([key, val]) => `${key}=${val ? 1 : 0}`)
-				.join(','),
-		],
-		{
-			env: {
-				...process.env,
-				root:                 __dirname,
-				NODE_ENV:             'development',
-				SKIP_PREFLIGHT_CHECK: true,
-				PORT:                 process.env.PORT || 3000,
+				'--procfile', path.resolve(__dirname, 'Procfile'),
+				Object.entries({ web, server, mongod })
+					.map(([key, val]) => `${key}=${val ? 1 : 0}`)
+					.join(','),
+			],
+			{
+				env: {
+					MONGO_URL:            mongod && 'mongodb://localhost:27017/database',
+					SKIP_PREFLIGHT_CHECK: true,
+					...process.env,
+					root:                 __dirname,
+					NODE_ENV:             'development',
+					PORT:                 process.env.PORT || 3000,
+				},
 			},
-			stdio: [process.stdin, process.stdout, process.stderr],
-		},
-	))
+		)))
 	.parse(process.argv);
