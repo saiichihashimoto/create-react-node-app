506d187dad67e02faf531c784b88baeb766e0657
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2019-02-10 15:58:37 -0800

    fix(code): allows src/index.server.js
    probably should allow the other extensions... ¯\_(ツ)_/¯
    
    fixes #7

--- bin/react-node-scripts-start.js
+++ bin/react-node-scripts-start.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const execa = require('execa');
+const fs = require('fs');
 const path = require('path');
 const program = require('commander');
 const { existsSync } = require('fs');
@@ -10,42 +11,49 @@ program
 	.option('--no-web')
 	.option('--no-server')
 	.action(
-		({ web, server, mongod }) => Promise.resolve()
+		process.env.NODE_ENV === 'production' ?
+			() => execa(
+				'node',
+				[
+					fs.existsSync('./lib/index.server.js') ? 'lib/index.server' : 'lib',
+				],
+				{
+					stdio: [process.stdin, process.stdout, process.stderr],
+				},
+			) :
+			({ web, server, mongod }) => Promise.resolve()
 			// This triggers it to download the binary
-			.then(() => mongod && !existsSync(path.resolve(homedir(), '.mongodb-prebuilt')) && execa(
-				'mongod',
-				[
-					'--version',
-				],
-				{
-					stdio: [process.stdin, process.stdout, process.stderr],
-				},
-			))
-			.then(() => execa(
-				'nf',
-				[
-					'start',
-
-					'--procfile', path.resolve(__dirname, 'Procfile'),
-					Object.entries({ web, server, mongod })
-						.map(([key, val]) => `${key}=${val ? 1 : 0}`)
-						.join(','),
-				],
-				{
-					env: {
-						MONGO_URL:            mongod && 'mongodb://localhost:27017/database',
-						SKIP_PREFLIGHT_CHECK: true,
-						...process.env,
-						root:                 __dirname,
-						NODE_ENV:             'development',
-						PORT:                 process.env.PORT || 3000,
+				.then(() => mongod && !existsSync(path.resolve(homedir(), '.mongodb-prebuilt')) && execa(
+					'mongod',
+					[
+						'--version',
+					],
+					{
+						stdio: [process.stdin, process.stdout, process.stderr],
 					},
-					stdio: [process.stdin, process.stdout, process.stderr],
-				},
-			))
-			.then(
-				({ stdout }) => stdout && console.log(stdout), // eslint-disable-line no-console
-				({ stderr }) => stderr && console.error(stderr), // eslint-disable-line no-console
-			),
+				))
+				.then(() => execa(
+					'nf',
+					[
+						'start',
+
+						'--procfile', path.resolve(__dirname, 'Procfile'),
+						Object.entries({ web, server, mongod })
+							.map(([key, val]) => `${key}=${val ? 1 : 0}`)
+							.join(','),
+					],
+					{
+						env: {
+							MONGO_URL:            mongod && 'mongodb://localhost:27017/database',
+							SKIP_PREFLIGHT_CHECK: true,
+							...process.env,
+							src:                  fs.existsSync('./src/index.server.js') ? 'src/index.server' : 'src',
+							PORT:                 process.env.PORT || 3000,
+							NODE_ENV:             'development',
+							root:                 __dirname,
+						},
+						stdio: [process.stdin, process.stdout, process.stderr],
+					},
+				)),
 	)
 	.parse(process.argv);
