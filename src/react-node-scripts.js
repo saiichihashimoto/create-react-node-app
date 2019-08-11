d8993b53cc1dbc8cabe456d16fe731e4eeb3546a
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2019-08-10 18:35:37 -0700

    refactor(cli): all cli logic in react-node-scripts

--- src/react-node-scripts.js
+++ src/react-node-scripts.js
@@ -1,21 +1,61 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
+/* istanbul ignore file */
 import updateNotifier from 'update-notifier';
+import { Command } from 'commander';
 
 import pkg from '../package';
 
-const reactNodeScripts = (args) => new Command()
-	.version(pkg.version)
-	.command('build', 'builds the app')
-	.command('start', 'starts the app')
-	.command('test', 'tests using `react-scripts test`')
-	.parse(args);
+import reactNodeScriptsBuild from './react-node-scripts-build';
+import reactNodeScriptsStart from './react-node-scripts-start';
+import reactNodeScriptsTest from './react-node-scripts-test';
 
-/* istanbul ignore next line */
-if (require.main === module) {
-	updateNotifier({ pkg }).notify();
+updateNotifier({ pkg }).notify();
 
-	reactNodeScripts(process.argv);
-}
+let action;
 
-export default (...args) => reactNodeScripts([process.argv[0], __filename, ...args]);
+const program = new Command()
+	.version(pkg.version);
+
+program
+	.command('build')
+	.option('--no-web')
+	.option('--no-node')
+	.action((...args) => { action = () => reactNodeScriptsBuild(...args); });
+
+program
+	.command('start')
+	.option('--mongod')
+	.option('--redis')
+	.option('--ngrok')
+	.option('--no-web')
+	.option('--no-node')
+	.action((...args) => { action = () => reactNodeScriptsStart(...args); });
+
+program
+	.command('test [args...]')
+	.action((...args) => { action = () => reactNodeScriptsTest(...args); });
+
+program.parse(process.argv);
+
+action()
+	.catch((err) => { /* eslint-disable-line promise/prefer-await-to-callbacks */
+		const queue = [err];
+
+		while (queue.length) {
+			const currentErr = queue.shift();
+
+			if (currentErr.errors) {
+				queue.push(...currentErr.errors);
+			} else if (currentErr.all) {
+				console.log(currentErr.all); /* eslint-disable-line no-console */
+			} else if (currentErr.stderr) {
+				console.error(currentErr.stderr); /* eslint-disable-line no-console */
+			} else if (currentErr.stdout) {
+				console.log(currentErr.stdout); /* eslint-disable-line no-console */
+			} else if (currentErr.message !== 'Staged Failed') {
+				console.error(currentErr); /* eslint-disable-line no-console */
+			}
+		}
+
+		process.exit(1);
+	});
